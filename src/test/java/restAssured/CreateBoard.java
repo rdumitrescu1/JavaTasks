ac9b0cef104f42a6dc723ee2c5a6a8f86586764d
rdumitrescu1 <139134890+rdumitrescu1@users.noreply.github.com>
2023-11-03 15:25:07 +0200

    Update CreateBoard.java

--- src/test/java/restAssured/CreateBoard.java
+++ src/test/java/restAssured/CreateBoard.java
@@ -7,34 +7,40 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pojoClasses.Board;
 
+//public class CreateBoard extends BaseTest{
 
 public class CreateBoard {
 
     @Test
     public void CreateBoardTest() {
-        try {
+        try {  //get rid of try/catch
             Board newBoard = new Board("Board by RestAssured", "This is created by using RestAssured", "651be0cea5cb14826012f007");
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            String body = objectMapper.writeValueAsString(newBoard);
+            ObjectMapper objectMapper = new ObjectMapper(); //move in BaseTest
+            String body = objectMapper.writeValueAsString(newBoard); //incapsulate in buildRequest
 
             Response response =
                     RestAssured
                             .given()
                             .contentType("application/json")
                             .body(body)
-                            .when()
+                            .when()  //Extract in buildRequestSepcifications()
                             .post("https://api.trello.com/1/boards?key=ea47e302b77fcbdc6bc095d3c632d405&token=ATTAdac1df17c5d268b1c8eef82325adc4867cd342c5c51f5963d9b9af68fffc89d8454F54BC")
                             .then()
                             //   .log().all()
-                            .extract()
+                            .extract()  //extract in sendPostRequest() below
                             .response();
+
+            //CreateResponsePojo actualResponse = response.as(CreateResponsePojo.class);
 
             System.out.println("Response Status Code: " + response.getStatusCode());
             System.out.println("Response Body: " + response.getBody().asString());
 
             Assert.assertEquals(response.getStatusCode(), 200, "Expected status code 200");
             Assert.assertNotNull(response.getBody().asString(), "Response body should not be null");
+            
+            //Assertions.assertThat(actualResponse.getSomething()).isEqualtTo(something);
+            
 
         } catch (Exception e) {
             e.printStackTrace();
