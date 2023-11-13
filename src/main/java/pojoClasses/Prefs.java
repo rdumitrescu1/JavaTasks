ce3b8e426fe3be7c97d5be838dffdec74fb0d935
rdumitrescu1 <139134890+rdumitrescu1@users.noreply.github.com>
2023-11-13 11:55:39 +0200

    Update Prefs.java

--- src/main/java/pojoClasses/Prefs.java
+++ src/main/java/pojoClasses/Prefs.java
@@ -1,28 +1,36 @@
-package pojoClasses; 
-import java.util.ArrayList;
-public class Prefs{
-    public String permissionLevel;
-    public boolean hideVotes;
-    public String voting;
-    public String comments;
-    public String invitations;
-    public boolean selfJoin;
-    public boolean cardCovers;
-    public boolean isTemplate;
-    public String cardAging;
-    public boolean calendarFeedEnabled;
-    public ArrayList<Object> hiddenPluginBoardButtons;
-    public String background;
-    public String backgroundColor;
-    public Object backgroundImage;
-    public Object backgroundImageScaled;
-    public boolean backgroundTile;
-    public String backgroundBrightness;
-    public String backgroundBottomColor;
-    public String backgroundTopColor;
-    public boolean canBePublic;
-    public boolean canBeEnterprise;
-    public boolean canBeOrg;
-    public boolean canBePrivate;
-    public boolean canInvite;
+package pojoClasses;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class Prefs {
+    private String permissionLevel;
+    private boolean hideVotes;
+    private String voting;
+    private String comments;
+    private String invitations;
+    private boolean selfJoin;
+    private boolean cardCovers;
+    private boolean isTemplate;
+    private String cardAging;
+    private boolean calendarFeedEnabled;
+    private List<Object> hiddenPluginBoardButtons;
+    private List<SwitcherView> switcherViews;
+    private String background;
+    private String backgroundColor;
+    private Object backgroundImage;
+    private Object backgroundImageScaled;
+    private boolean backgroundTile;
+    private String backgroundBrightness;
+    private String backgroundBottomColor;
+    private String backgroundTopColor;
+    private boolean canBePublic;
+    private boolean canBeEnterprise;
+    private boolean canBeOrg;
+    private boolean canBePrivate;
+    private boolean canInvite;
+
+
 }
+
