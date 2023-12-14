2f643adb617eb4b7e4bffe67727eac268cb93100
rdumitrescu1 <139134890+rdumitrescu1@users.noreply.github.com>
2023-12-14 15:12:14 +0200

    Update GetBoard.java

--- src/test/java/restAssured/GetBoard.java
+++ src/test/java/restAssured/GetBoard.java
@@ -13,11 +13,12 @@ public class GetBoard extends BaseTest {
     public void GetBoardTest() {
 
         Response response = sendGetRequest("65324d421c4b3d39e647dc0c");
-        Board actualResponse = convertJsonToObject(response);
+        Board actualResponse = response.as(Board.class);
 
         checkStatusCode(response, 200);
         Assertions.assertThat(actualResponse.getName()).isEqualTo("Razvan's board");
         Assertions.assertThat(actualResponse.getDesc()).isEqualTo("This board it's empty");
         Assert.assertNotNull(actualResponse, "Response body should not be null");
+
     }
 }
